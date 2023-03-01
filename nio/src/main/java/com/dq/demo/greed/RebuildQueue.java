9863f9460b5b5e291eed679ac122fea78bdcea63
DDSDB <954716349@qq.com>
2023-03-01 13:43:14 +0800

    feature: 增加netty demo

---
+++ nio/src/main/java/com/dq/demo/greed/RebuildQueue.java
@@ -0,0 +1,67 @@
+package com.dq.demo.greed;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。
+ * 每个 people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
+ * <p>
+ * 请你重新构造并返回输入数组people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第 j 个人的属性（queue[0] 是排在队列前面的人）
+ * 4,4 5,0 5,2 6,1 7,0 7,1
+ * 输入：people =  7,1
+ * 输出：[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+ * 解释：
+ * 编号为 0 的人身高为 5 ，没有身高更高或者相同的人排在他前面。
+ * 编号为 1 的人身高为 7 ，没有身高更高或者相同的人排在他前面。
+ * 编号为 2 的人身高为 5 ，有 2 个身高更高或者相同的人排在他前面，即编号为 0 和 1 的人。
+ * 编号为 3 的人身高为 6 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+ * 编号为 4 的人身高为 4 ，有 4 个身高更高或者相同的人排在他前面，即编号为 0、1、2、3 的人。
+ * 编号为 5 的人身高为 7 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+ * 因此 [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] 是重新构造后的队列。
+ * <p>
+ * <p>
+ * 输入：people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+ * 输出：[[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+ * <p>
+ * 1 <= people.length <= 2000
+ * 0 <= hi <= 106
+ * 0 <= ki < people.length
+ * 题目数据确保队列可以被重建
+ */
+public class RebuildQueue {
+    public static void main(String[] args) {
+        // [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+        int[][] people = new int[][]{new int[]{6, 0}, new int[]{5, 0}, new int[]{4, 0}, new int[]{3, 2}, new int[]{2, 2}, new int[]{1, 4}};
+//        int[][] people = new int[][]{new int[]{7, 0}, new int[]{4, 4}, new int[]{7, 1}, new int[]{5, 0}, new int[]{6, 1}, new int[]{5, 2}};
+//        int[][] people = new int[][]{new int[]{7, 0}, new int[]{4, 4}, new int[]{7, 1}, new int[]{5, 0}, new int[]{6, 1}, new int[]{5, 2}};
+        int[][] ints = reconstructQueue(people);
+        for (int[] anInt : ints) {
+            System.out.println(Arrays.toString(anInt));
+        }
+    }
+
+
+    public static int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            } else {
+                return o2[1] - o1[1];
+            }
+        });
+
+        if (people.length < 2) {
+            return people;
+        }
+        List<int[]> result = new ArrayList<>(people.length);
+        result.add(people[people.length - 1]);
+        for (int i = people.length - 2; i >= 0; i--) {
+            int[] person = people[i];
+            result.add(person[1], person);
+        }
+        return result.toArray(new int[][]{});
+    }
+}
